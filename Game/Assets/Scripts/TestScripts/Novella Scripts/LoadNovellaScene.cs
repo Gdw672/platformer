e472f24eb7f0c7724f89d654abab71dec79fe5bb
Gdw672 <van_der_linde90@mail.ru>
2022-10-31 23:03:19 +0300

    +fungus

---
+++ Game/Assets/Scripts/TestScripts/Novella Scripts/LoadNovellaScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNovellaScene : MonoBehaviour
+{
+   
+    void Start()
+    {
+      //  SceneManager.LoadScene("NovellaScene", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
